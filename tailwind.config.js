643e7ecfe00f6f88e580a4d15efa5f89d7034eb3
ghelgameshra <ghelgameshra3347@gmail.com>
2023-09-26 03:02:34 +0700

    view: (header, section, pegalaman)

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+    darkMode: 'class',
     content: ["./**/*.{html,js}"],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                'main': '#0f172a',
+                'dark': '#1e293bff',
+                'secondary': '#183351',
+            }
+        },
     },
     plugins: [],
 }
